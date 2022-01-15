d4c3a91f7a68ccb1fcb4666b8063f1125f9c8bb7
Stefen Phelps <stefen@kelp.agency>
2022-01-15 10:04:22 -0500

    blah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
+export default {
 	projectRoot: '.', // Where to resolve all URLs relative to. Useful if you have a monorepo project.
 	pages: './src/pages', // Path to Astro components, pages, and data
 	dist: './dist', // When running `astro build`, path to final static output
@@ -19,5 +10,5 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
 	devOptions: {
 		hostname: 'localhost', // The hostname to run the dev server on.
 		port: 3000, // The port to run the dev server on.
-	}
-});
+	},
+};
