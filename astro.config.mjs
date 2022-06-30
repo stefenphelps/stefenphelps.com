124249d17cefe3d36782eb2d385a1de8cbbf047d
Stefen Phelps <stefen@kelp.agency>
2022-06-30 15:38:28 -0400

    💩

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,38 @@ export default defineConfig({
 	site: 'https://stefenphelps.com/',
 	trailingSlash: 'always',
 	vite: {
-		plugins: [VitePWA()]
+		plugins: [
+			VitePWA({
+				manifest: {
+					name: 'Stefen Phelps',
+					short_name: 'Stefen',
+					description:
+						"Stefen's Personal website where he writes about web development and sometimes other things.",
+					start_url: 'https://stefenphelps.com',
+					theme_color: '#473b2c',
+					background_color: '#473b2c',
+					display: 'standalone',
+					icons: [
+						{
+							src: '/pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png'
+						},
+						{
+							src: '/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png'
+						},
+						{
+							src: '/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any maskable'
+						}
+					]
+				}
+			})
+		]
 	},
 	markdown: {
 		syntaxHighlight: 'prism'
