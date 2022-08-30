4bcbe2b06e86078279573f36868121f6a4e4c22f
Stefen Phelps <stefen@kelp.agency>
2022-08-30 09:43:16 -0400

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,9 @@ import rss from '@astrojs/rss';
 
 const postImportResult = import.meta.globEager('./blog/**/*.md');
 const posts = Object.values(postImportResult);
+const sortedPosts = posts.sort(
+	(a, b) => new Date(b.frontmatter.publishDate).valueOf() - new Date(a.frontmatter.publishDate).valueOf()
+);
 
 // Generate an RSS feed from this collection of posts.
 export const get = () =>
@@ -10,7 +13,7 @@ export const get = () =>
 		description: 'My thoughts on web development and sometimes other things.',
 		site: import.meta.env.SITE,
 		customData: `<language>en-us</language>`,
-		items: posts.map((item) => ({
+		items: sortedPosts.map((item) => ({
 			title: item.frontmatter.title,
 			description: item.frontmatter.description,
 			link: item.url,
