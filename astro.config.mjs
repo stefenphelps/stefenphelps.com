8183c24ecc5360c60045a213dea25ab9ddce5004
Stefen Phelps <stefen@kelp.agency>
2022-09-22 21:36:29 -0400

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,54 +5,64 @@ import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import critters from 'astro-critters';
 
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stefenphelps.com/',
-  trailingSlash: 'always',
-  vite: {
-    plugins: [VitePWA({
-      strategies: 'generateSW',
-      workbox: {
-        globPatterns: ['**/*.{js,css}'],
-        navigateFallback: null
-      },
-      manifest: {
-        name: 'Stefen Phelps',
-        short_name: 'Stefen',
-        description: "Stefen's Personal website where he writes about web development and sometimes other things.",
-        start_url: 'https://stefenphelps.com',
-        theme_color: '#473b2c',
-        background_color: '#473b2c',
-        display: 'standalone',
-        icons: [{
-          src: '/pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        }, {
-          src: '/pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }, {
-          src: '/pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable'
-        }]
-      }
-    })]
-  },
-  markdown: {
-    syntaxHighlight: 'prism'
-  },
-  integrations: [sitemap(), astroImageTools, compress({
-    css: true,
-    html: true,
-    js: true,
-    img: false,
-    svg: false
-  }), critters(), prefetch()]
+	site: 'https://stefenphelps.com/',
+	trailingSlash: 'always',
+	vite: {
+		plugins: [
+			VitePWA({
+				strategies: 'generateSW',
+				workbox: {
+					globPatterns: ['**/*.{js,css}'],
+					navigateFallback: null
+				},
+				manifest: {
+					name: 'Stefen Phelps',
+					short_name: 'Stefen',
+					description: "Stefen's Personal website where he writes about web development and sometimes other things.",
+					start_url: 'https://stefenphelps.com',
+					theme_color: '#473b2c',
+					background_color: '#473b2c',
+					display: 'standalone',
+					icons: [
+						{
+							src: '/pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png'
+						},
+						{
+							src: '/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png'
+						},
+						{
+							src: '/pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+							purpose: 'any maskable'
+						}
+					]
+				}
+			})
+		]
+	},
+	markdown: {
+		syntaxHighlight: 'prism'
+	},
+	integrations: [
+		sitemap(),
+		astroImageTools,
+		compress({
+			css: true,
+			html: true,
+			js: true,
+			img: false,
+			svg: false
+		}),
+		critters(),
+		prefetch()
+	]
 });
-
-astroImageTools.hooks['astro:build:done'] = async () => (await import('./node_modules/astro-imagetools/plugin/hooks/closeBundle.js')).default();
