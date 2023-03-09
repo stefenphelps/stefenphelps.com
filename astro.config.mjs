80c2e9e6a4dff7d54418e15734f95c0092e8a7ed
Stefen Phelps <stefen@kelp.agency>
2023-03-09 09:41:31 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -61,7 +61,7 @@ export default defineConfig({
 			},
 			workbox: {
 				navigateFallback: null,
-				globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,avif,jpeg}']
+				globPatterns: ['**/*.{css,js,html,svg,ico,txt,avif}']
 			},
 			devOptions: {
 				enabled: true
