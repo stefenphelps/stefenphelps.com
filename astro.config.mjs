a2a5115314425978a4d2aa9d137215aee89cc05e
Stefen Phelps <stefen@kelp.agency>
2023-07-11 09:56:42 -0400

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,54 +7,54 @@ import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stefenphelps.com/',
-  trailingSlash: 'always',
-  compressHTML: true,
-  markdown: {
-    shikiConfig: {
-      theme: 'material-darker',
-    },
-  },
-  integrations: [
-    sitemap(),
-    astroImageTools,
-    critters(),
-    prefetch(),
-    AstroPWA({
-      base: '/',
-      scope: '/',
-      includeAssets: ['fonts/*.woff2', '*.{png,ico,svg,jpg,xml}'],
-      manifest: {
-        name: 'Stefen Phelps',
-        short_name: 'Stefen',
-        description:
-          "Stefen's Personal website where he writes about web development and sometimes other things.",
-        start_url: 'https://stefenphelps.com',
-        theme_color: '#473b2c',
-        background_color: '#473b2c',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
+	site: 'https://stefenphelps.com/',
+	trailingSlash: 'always',
+	compressHTML: false,
+	markdown: {
+		shikiConfig: {
+			theme: 'material-darker'
+		}
+	},
+	integrations: [
+		sitemap(),
+		astroImageTools,
+		critters(),
+		prefetch(),
+		AstroPWA({
+			base: '/',
+			scope: '/',
+			includeAssets: ['fonts/*.woff2', '*.{png,ico,svg,jpg,xml}'],
+			manifest: {
+				name: 'Stefen Phelps',
+				short_name: 'Stefen',
+				description:
+					"Stefen's Personal website where he writes about web development and sometimes other things.",
+				start_url: 'https://stefenphelps.com',
+				theme_color: '#473b2c',
+				background_color: '#473b2c',
+				display: 'standalone',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				]
+			},
+			devOptions: {
+				enabled: true
+			}
+		})
+	]
 });
