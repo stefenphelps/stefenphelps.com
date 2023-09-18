7dda41b6fafe1592db7b0c897dbc53d68d0c4ae8
Stefen Phelps <stefen@kelp.agency>
2023-09-18 09:51:42 -0400

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,16 @@
 import { defineConfig, sharpImageService } from 'astro/config';
-// import AstroPWA from '@vite-pwa/astro';
 import sitemap from '@astrojs/sitemap';
 import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stefenphelps.com/',
-  trailingSlash: 'always',
-  compressHTML: false,
-  markdown: {
-    shikiConfig: {
-      theme: 'material-theme-darker',
-    },
-  },
-  integrations: [
-    sitemap(),
-    prefetch(),
-    // AstroPWA({
-    //   base: '/',
-    //   scope: '/',
-    //   includeAssets: ['fonts/*.woff2', '*.{png,ico,svg,jpg,xml}'],
-    //   manifest: {
-    //     name: 'Stefen Phelps',
-    //     short_name: 'Stefen',
-    //     description:
-    //       "Stefen's Personal website where he writes about web development and sometimes other things.",
-    //     start_url: 'https://stefenphelps.com',
-    //     theme_color: '#473b2c',
-    //     background_color: '#473b2c',
-    //     display: 'standalone',
-    //     icons: [
-    //       {
-    //         src: '/pwa-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/pwa-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable',
-    //       },
-    //     ],
-    //   },
-    // }),
-  ],
+	site: 'https://stefenphelps.com/',
+	trailingSlash: 'always',
+	compressHTML: false,
+	markdown: {
+		shikiConfig: {
+			theme: 'material-theme-darker'
+		}
+	},
+	integrations: [sitemap(), prefetch()]
 });
